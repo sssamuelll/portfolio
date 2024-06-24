0afe5228b2a6897fcec0b9915e8256cc15d423a0
Samuel Ballesteros <sssamuelll@gmail.com>
2024-06-24 09:56:14 +0000

    hola mundo tres

--- src/components/svg_links/SvgLinks.tsx
+++ src/components/svg_links/SvgLinks.tsx
@@ -5,21 +5,21 @@ import Instagram from '../../assets/svgs/Instagram'
 import Github from '../../assets/svgs/Github'
 
 const SvgLinks: React.FC = () => {
-    const height = 20
-    const width = 20
+    const height = 30
+    const width = 30
     return (
         <div className="links">
-            <a className="link" href="mailto:correo@samueldar.io">
-                <Envelope height={height} width={width} />
-            </a>
-            <a className="link" href="https://github.com/sssamuelll">
-                <Github height={height} width={width} />
-            </a>
             <a className="link" href="https://linkedin.com/in/samueldarioballesteros">
                 <LinkedIn height={height} width={width} />
             </a>
             <a className="link" href="https://instagram.com/samueldar.io">
                 <Instagram height={height} width={width} />
+            </a>
+            <a className="link" href="https://github.com/sssamuelll">
+                <Github height={height} width={width} />
+            </a>
+            <a className="link" href="mailto:correo@samueldar.io">
+                <Envelope height={height} width={width} />
             </a>
         </div>
     )
