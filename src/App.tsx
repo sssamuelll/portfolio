72b6ee2e9c73dfc61c9b94b84b696275c84a8fec
sssamuelll <sssamuelll@gmail.com>
2025-09-21 08:14:45 +0200

    delete deprecated components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './styles/App.css';
-import CVPage from './pages/CVPage/CVPage';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/cv" element={<CVPage />} />
             </Routes>
         </Router>
     );
